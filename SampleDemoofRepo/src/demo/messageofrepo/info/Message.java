5dc77c8f5f4aa18e6d3ecc3cde663c81bc475c09
vishal <vishal.thorat8860@gmail.com>
2023-06-26 23:04:11 +0530

    firstsampledemorepoprojectexample

---
+++ SampleDemoofRepo/src/demo/messageofrepo/info/Message.java
@@ -0,0 +1,8 @@
+package demo.messageofrepo.info;
+
+public class Message {
+	public static void main(String[] args) {
+		System.out.println("vishal-thorat-allproject code are test on repo files ");
+	}
+
+}
